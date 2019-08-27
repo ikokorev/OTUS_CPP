b3432aad2b3341d5e1e7dea1590f7ecef86be868
gwyn0431 <gwyn0431@gmail.com>
2019-08-27 11:31:39 -0800

    A-03. Custom allocator version which crashes on first deallocation.

---
+++ A-03/CustomAllocator.h
@@ -0,0 +1,82 @@
+#include <memory>
+#include <iostream>
+#include <type_traits>
+#include <array>
+#include <stack>
+
+template<typename T, size_t Size>
+struct CustomAllocator 
+{
+    using value_type = T;
+
+    using pointer = T*;
+    using const_pointer = const T*;
+    using reference = T&;
+    using const_reference = const T&;
+
+    template<typename U>
+    struct rebind 
+    {
+        using other = CustomAllocator<U, Size>;
+    };
+
+    CustomAllocator() = default;
+    ~CustomAllocator() = default;
+
+    template<typename U> 
+    CustomAllocator(const CustomAllocator<U, Size>&) 
+    {
+    }
+
+    pointer allocate(std::size_t n) 
+    {
+        std::cout << "\nAllocating " << n << " element(s)." << std::endl;
+
+        if (!IsMemoryReserved)
+        {
+            std::cout << "Reserving memory for " << Size << " elements. " 
+            << "Value type sizeof is - " << sizeof(value_type) << std::endl;
+            
+            IsMemoryReserved = true;
+            MemoryStartPtr = std::malloc(n * Size * sizeof(value_type));
+        }
+        
+        if (Offset < Size)
+        {
+            std::cout << "Correct allocation with offset - " << Offset << std::endl;
+            return reinterpret_cast<pointer>(MemoryStartPtr) + sizeof(value_type) * Offset++ * n;
+        }
+        else
+        {
+            std::cout << "Bad Allocation, allocator reserved memory for " << Size << " element(s), " 
+            << "you trying to add element No - " << Offset + 1 << std::endl;
+            throw std::bad_alloc();            
+        }
+    }
+
+    void deallocate(pointer p, std::size_t n) 
+    {
+        std::cout << "Deallocating " << n << " element(s)." << std::endl;
+        std::free(p);
+    }
+
+    template<typename U, typename... Args>
+    void construct(U* p, Args&&... args) 
+    {
+        std::cout << "Construct" << std::endl;
+        new(p) U(std::forward<Args>(args)...);
+    };
+
+    void destroy(pointer p) 
+    {
+        std::cout << "Destroy" << std::endl;
+        p->~T();
+    }
+
+private:
+
+    void* MemoryStartPtr;
+    bool IsMemoryReserved = false;
+    size_t Offset = 0;
+};
+
---
+++ A-03/Main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "CustomAllocator.h"
+#include <map>
+#include <list>
+#include <vector>
+
+size_t Factorial(size_t Number)
+{
+    if(Number < 0)
+    {
+        return 0;
+    } 
+        
+    if (Number == 0)
+    {
+        return 1;
+    }
+        
+    return Number * Factorial(Number - 1);
+}
+
+int main()
+{
+    const size_t AllocSize = 10; 
+    std::map<int, int, std::less<int>, CustomAllocator<std::pair<const int, int>, AllocSize>> FactorialValues;
+    
+    // Full filling of the allocator by its size causes bad allocation exception, e.g to fill allocator with
+    // 10 elements I have to use CustomAllocator with size equals to 11. So here I substract 1 from AllocSize,
+    // cause its temporarly irrelevant (I have to figure out, how to create a valid allocator with reserve first).
+    for (size_t i = 0; i < AllocSize - 1; ++i) 
+    {
+        FactorialValues.emplace(i, Factorial(i));
+        std::cout << "\nAdded element to map, its value - " << FactorialValues.at(i) << std::endl;
+    }
+
+    for (auto Elem : FactorialValues)
+    {
+        std::cout << Elem.second << std::endl;
+    }
+
+    return 0;
+}
