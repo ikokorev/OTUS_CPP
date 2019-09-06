12d78b71ac81b30a07b83a64e71a700e9e8164f9
gwyn0431 <gwyn0431@gmail.com>
2019-09-06 13:11:39 -0800

    A-03. Testing old tests code to see if it works. I'm getting segfault only on build machine somehow.

--- A-03/CustomAllocator/CustomAllocatorTests.cpp
+++ A-03/CustomAllocator/CustomAllocatorTests.cpp
@@ -1,80 +1,112 @@
 #include "gtest/gtest.h"
 #include "CustomAllocator.h"
+#include "ForwardList/ForwardList.h"
 
-TEST(CustomAllocatorTests, TestAllocByItself) 
+// TEST(CustomAllocatorTests, TestAllocByItself) 
+// {
+//     CustomAllocator<int, 1> IntAllocator;
+//     int* IntPtr = IntAllocator.allocate(1);
+//     IntAllocator.construct(IntPtr, 15);
+
+//     //EXPECT_EQ(*IntPtr, 15);
+
+//     IntAllocator.destroy(IntPtr);
+//     IntAllocator.deallocate(IntPtr, 1);
+
+//     // second verification exists to check if allocator can validly allocate again, after deallocation
+//     IntPtr = IntAllocator.allocate(1);
+//     IntAllocator.construct(IntPtr, 10);
+
+//     //EXPECT_EQ(*IntPtr, 10);
+// }
+
+// TEST(CustomAllocatorTests, TestAllocExpanding) 
+// {
+//     CustomAllocator<int, 1> IntAllocator;
+    
+//     int* FirstIntPtr = IntAllocator.allocate(1);
+//     IntAllocator.construct(FirstIntPtr, 1);
+//     int* SecondIntPtr = IntAllocator.allocate(1);
+//     IntAllocator.construct(SecondIntPtr, 2);
+
+//     //EXPECT_EQ(*FirstIntPtr, 1);
+//     //EXPECT_EQ(*SecondIntPtr, 2);
+
+//     IntAllocator.destroy(FirstIntPtr);
+//     IntAllocator.deallocate(FirstIntPtr, 1);
+//     IntAllocator.destroy(SecondIntPtr);
+//     IntAllocator.deallocate(SecondIntPtr, 1);
+
+//     // second verification exists to check if expanded allocator can validly allocate again, after deallocation
+//     FirstIntPtr = IntAllocator.allocate(1);
+//     IntAllocator.construct(FirstIntPtr, 10);
+//     SecondIntPtr = IntAllocator.allocate(1);
+//     IntAllocator.construct(SecondIntPtr, 20);
+
+//     //EXPECT_EQ(*FirstIntPtr, 10);
+//     //EXPECT_EQ(*SecondIntPtr, 20);
+// }
+
+// TEST(CustomAllocatorTests, TestAllocWithMap) 
+// {
+//     std::map<int, int, std::less<int>, CustomAllocator<std::pair<const int, int>, 5>> Map;
+
+//     for (int Value = 0; Value < 5; ++Value)
+//     {
+//         Map.emplace(Value, Value);
+//     }
+
+//     int Value = 0;
+//     for (auto Pair : Map)
+//     {
+//         EXPECT_EQ(Pair.second, Value++);
+//     }
+// }
+
+// TEST(CustomAllocatorTests, TestAllocExpandingWithMap) 
+// {
+//     std::map<int, int, std::less<int>, CustomAllocator<std::pair<const int, int>, 1>> Map;
+
+//     for (int Value = 0; Value < 5; ++Value)
+//     {
+//         Map.emplace(Value, Value);
+//     }
+
+//     int Value = 0;
+//     for (auto Pair : Map)
+//     {
+//         EXPECT_EQ(Pair.second, Value++);
+//     }
+// }
+
+TEST(CustomAllocatorTests, TestAllocWithMap) 
 {
-    CustomAllocator<int, 1> IntAllocator;
-    int* IntPtr = IntAllocator.allocate(1);
-    IntAllocator.construct(IntPtr, 15);
+    std::map<int, int, std::less<int>, CustomAllocator<int, 1>> Map;
+    Map.emplace(1, 1);
 
-    //EXPECT_EQ(*IntPtr, 15);
+    EXPECT_EQ(Map.at(1), 1);
+}
 
-    IntAllocator.destroy(IntPtr);
-    IntAllocator.deallocate(IntPtr, 1);
+TEST(CustomAllocatorTests, TestAllocWithForwardList) 
+{
+    ForwardList<int, CustomAllocator<int, 1>> List;
+    List.Push(1);
 
-    // second verification exists to check if allocator can validly allocate again, after deallocation
-    IntPtr = IntAllocator.allocate(1);
-    IntAllocator.construct(IntPtr, 10);
-
-    //EXPECT_EQ(*IntPtr, 10);
+    EXPECT_EQ(*List.begin(), 1);
 }
 
 TEST(CustomAllocatorTests, TestAllocExpanding) 
 {
-    CustomAllocator<int, 1> IntAllocator;
-    
-    int* FirstIntPtr = IntAllocator.allocate(1);
-    IntAllocator.construct(FirstIntPtr, 1);
-    int* SecondIntPtr = IntAllocator.allocate(1);
-    IntAllocator.construct(SecondIntPtr, 2);
-
-    //EXPECT_EQ(*FirstIntPtr, 1);
-    //EXPECT_EQ(*SecondIntPtr, 2);
-
-    IntAllocator.destroy(FirstIntPtr);
-    IntAllocator.deallocate(FirstIntPtr, 1);
-    IntAllocator.destroy(SecondIntPtr);
-    IntAllocator.deallocate(SecondIntPtr, 1);
-
-    // second verification exists to check if expanded allocator can validly allocate again, after deallocation
-    FirstIntPtr = IntAllocator.allocate(1);
-    IntAllocator.construct(FirstIntPtr, 10);
-    SecondIntPtr = IntAllocator.allocate(1);
-    IntAllocator.construct(SecondIntPtr, 20);
-
-    //EXPECT_EQ(*FirstIntPtr, 10);
-    //EXPECT_EQ(*SecondIntPtr, 20);
-}
-
-TEST(CustomAllocatorTests, TestAllocWithMap) 
-{
-    std::map<int, int, std::less<int>, CustomAllocator<std::pair<const int, int>, 5>> Map;
-
+    ForwardList<int, CustomAllocator<int, 1>> List;
     for (int Value = 0; Value < 5; ++Value)
     {
-        Map.emplace(Value, Value);
+        List.Push(Value);
     }
 
     int Value = 0;
-    for (auto Pair : Map)
+    for (ForwardIterator<int> Iter = List.begin(); Iter != List.end(); ++Iter)
     {
-        EXPECT_EQ(Pair.second, Value++);
-    }
-}
-
-TEST(CustomAllocatorTests, TestAllocExpandingWithMap) 
-{
-    std::map<int, int, std::less<int>, CustomAllocator<std::pair<const int, int>, 1>> Map;
-
-    for (int Value = 0; Value < 5; ++Value)
-    {
-        Map.emplace(Value, Value);
-    }
-
-    int Value = 0;
-    for (auto Pair : Map)
-    {
-        EXPECT_EQ(Pair.second, Value++);
+        EXPECT_EQ(*Iter, Value++);
     }
 }
 
