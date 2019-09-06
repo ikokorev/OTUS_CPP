895d162cf1017169584269101ea6d3d2cc54167b
gwyn0431 <gwyn0431@gmail.com>
2019-09-06 13:17:22 -0800

    A-03. Using binary search to detect the test, which causes segfault. Enabled half of the newest tests.

--- A-03/CustomAllocator/CustomAllocatorTests.cpp
+++ A-03/CustomAllocator/CustomAllocatorTests.cpp
@@ -2,50 +2,50 @@
 #include "CustomAllocator.h"
 #include "ForwardList/ForwardList.h"
 
-// TEST(CustomAllocatorTests, TestAllocByItself) 
-// {
-//     CustomAllocator<int, 1> IntAllocator;
-//     int* IntPtr = IntAllocator.allocate(1);
-//     IntAllocator.construct(IntPtr, 15);
+TEST(CustomAllocatorTests, TestAllocByItself) 
+{
+    CustomAllocator<int, 1> IntAllocator;
+    int* IntPtr = IntAllocator.allocate(1);
+    IntAllocator.construct(IntPtr, 15);
 
-//     //EXPECT_EQ(*IntPtr, 15);
+    EXPECT_EQ(*IntPtr, 15);
 
-//     IntAllocator.destroy(IntPtr);
-//     IntAllocator.deallocate(IntPtr, 1);
+    IntAllocator.destroy(IntPtr);
+    IntAllocator.deallocate(IntPtr, 1);
 
-//     // second verification exists to check if allocator can validly allocate again, after deallocation
-//     IntPtr = IntAllocator.allocate(1);
-//     IntAllocator.construct(IntPtr, 10);
+    // second verification exists to check if allocator can validly allocate again, after deallocation
+    IntPtr = IntAllocator.allocate(1);
+    IntAllocator.construct(IntPtr, 10);
 
-//     //EXPECT_EQ(*IntPtr, 10);
-// }
+    EXPECT_EQ(*IntPtr, 10);
+}
 
-// TEST(CustomAllocatorTests, TestAllocExpanding) 
-// {
-//     CustomAllocator<int, 1> IntAllocator;
+TEST(CustomAllocatorTests, TestAllocExpanding) 
+{
+    CustomAllocator<int, 1> IntAllocator;
     
-//     int* FirstIntPtr = IntAllocator.allocate(1);
-//     IntAllocator.construct(FirstIntPtr, 1);
-//     int* SecondIntPtr = IntAllocator.allocate(1);
-//     IntAllocator.construct(SecondIntPtr, 2);
+    int* FirstIntPtr = IntAllocator.allocate(1);
+    IntAllocator.construct(FirstIntPtr, 1);
+    int* SecondIntPtr = IntAllocator.allocate(1);
+    IntAllocator.construct(SecondIntPtr, 2);
 
-//     //EXPECT_EQ(*FirstIntPtr, 1);
-//     //EXPECT_EQ(*SecondIntPtr, 2);
+    EXPECT_EQ(*FirstIntPtr, 1);
+    EXPECT_EQ(*SecondIntPtr, 2);
 
-//     IntAllocator.destroy(FirstIntPtr);
-//     IntAllocator.deallocate(FirstIntPtr, 1);
-//     IntAllocator.destroy(SecondIntPtr);
-//     IntAllocator.deallocate(SecondIntPtr, 1);
+    IntAllocator.destroy(FirstIntPtr);
+    IntAllocator.deallocate(FirstIntPtr, 1);
+    IntAllocator.destroy(SecondIntPtr);
+    IntAllocator.deallocate(SecondIntPtr, 1);
 
-//     // second verification exists to check if expanded allocator can validly allocate again, after deallocation
-//     FirstIntPtr = IntAllocator.allocate(1);
-//     IntAllocator.construct(FirstIntPtr, 10);
-//     SecondIntPtr = IntAllocator.allocate(1);
-//     IntAllocator.construct(SecondIntPtr, 20);
+    // second verification exists to check if expanded allocator can validly allocate again, after deallocation
+    FirstIntPtr = IntAllocator.allocate(1);
+    IntAllocator.construct(FirstIntPtr, 10);
+    SecondIntPtr = IntAllocator.allocate(1);
+    IntAllocator.construct(SecondIntPtr, 20);
 
-//     //EXPECT_EQ(*FirstIntPtr, 10);
-//     //EXPECT_EQ(*SecondIntPtr, 20);
-// }
+    EXPECT_EQ(*FirstIntPtr, 10);
+    EXPECT_EQ(*SecondIntPtr, 20);
+}
 
 // TEST(CustomAllocatorTests, TestAllocWithMap) 
 // {
@@ -95,7 +95,7 @@ TEST(CustomAllocatorTests, TestAllocWithForwardList)
     EXPECT_EQ(*List.begin(), 1);
 }
 
-TEST(CustomAllocatorTests, TestAllocExpanding) 
+TEST(CustomAllocatorTests, TestAllocExpandingOld) 
 {
     ForwardList<int, CustomAllocator<int, 1>> List;
     for (int Value = 0; Value < 5; ++Value)
