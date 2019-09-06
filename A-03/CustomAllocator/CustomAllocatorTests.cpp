89f52eea3bed2718af9394efa58294bf1dd54a42
gwyn0431 <gwyn0431@gmail.com>
2019-09-06 13:23:52 -0800

    A-03. Fixed tests namings collision.

--- A-03/CustomAllocator/CustomAllocatorTests.cpp
+++ A-03/CustomAllocator/CustomAllocatorTests.cpp
@@ -79,37 +79,6 @@ TEST(CustomAllocatorTests, TestAllocWithMap)
 //     }
 // }
 
-TEST(CustomAllocatorTests, TestAllocWithMap) 
-{
-    std::map<int, int, std::less<int>, CustomAllocator<int, 1>> Map;
-    Map.emplace(1, 1);
-
-    EXPECT_EQ(Map.at(1), 1);
-}
-
-TEST(CustomAllocatorTests, TestAllocWithForwardList) 
-{
-    ForwardList<int, CustomAllocator<int, 1>> List;
-    List.Push(1);
-
-    EXPECT_EQ(*List.begin(), 1);
-}
-
-TEST(CustomAllocatorTests, TestAllocExpandingOld) 
-{
-    ForwardList<int, CustomAllocator<int, 1>> List;
-    for (int Value = 0; Value < 5; ++Value)
-    {
-        List.Push(Value);
-    }
-
-    int Value = 0;
-    for (ForwardIterator<int> Iter = List.begin(); Iter != List.end(); ++Iter)
-    {
-        EXPECT_EQ(*Iter, Value++);
-    }
-}
-
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
