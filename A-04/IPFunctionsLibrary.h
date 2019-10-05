afeb563f981313075eea94b7de896e5e1ed1d41f
gwyn0431 <gwyn0431@gmail.com>
2019-10-05 11:47:53 -0800

    A-04. Fixing build errors.

--- A-04/IPFunctionsLibrary.h
+++ A-04/IPFunctionsLibrary.h
@@ -35,7 +35,7 @@ typename std::enable_if<!std::is_same_v<ContainerType, std::string>, std::string
 ConvertIPToString(const ContainerType& IP)
 {
     std::string Result;
-    auto LastElement = --IP.cend();
+    auto LastElement = IP.cend() - 1;
 
     for (auto Iter = IP.cbegin(); Iter != LastElement; ++Iter)
     {
